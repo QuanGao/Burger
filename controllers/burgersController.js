d01af2909fe057f27d32d78b17216d57be0479fa
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-04 16:09:05 -0600

    link to js and jquery in main.handlebars

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,8 +5,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", function(req, res) {
-    res.send(data);
+router.get("/index", function(req, res) {
+    res.render(data);
 });
 
 router.post("/api/", function(req, res) {
